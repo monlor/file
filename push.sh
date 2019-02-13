3e3fe2b5b482747b317420bb9c59e3283e6f8225
monlor <monlor@qq.com>
2019-02-13 11:10:43 +0800

    2019-02-13

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-GitUrl="https://git.dev.tencent.com/monlor"
+GitUrl1="https://git.dev.tencent.com/monlor"
+GitUrl2="https://github.com/monlor"
 
 ProjectPath="$(pwd)"
 ProjectName="$(echo $ProjectPath | sed -e "s|/.*/||")"
@@ -7,14 +8,20 @@ ProjectName="$(echo $ProjectPath | sed -e "s|/.*/||")"
 echo "Add files to html list..."
 ./packhtml.sh
 
-[ -z "$ProjectName" ] && echo "Null Project Name!" && exit
-echo "Push [$ProjectName] To [$GitUrl/$ProjectName.git]."
-cd "$ProjectPath"
-find . -name '.DS_Store' | xargs rm -rf
-git add .
-git commit -m "`date +%Y-%m-%d`"
-git remote rm origin
-git remote add origin "$GitUrl"/"$ProjectName".git
-git push -u origin master -f
+function push() {
+	GitUrl="$1"
+	[ -z "$ProjectName" ] && echo "Null Project Name!" && exit
+	echo "Push [$ProjectName] To [$GitUrl/$ProjectName.git]."
+	cd "$ProjectPath"
+	find . -name '.DS_Store' | xargs rm -rf
+	git add .
+	git commit -m "`date +%Y-%m-%d`"
+	git remote rm origin
+	git remote add origin "$GitUrl"/"$ProjectName".git
+	git push -u origin master -f
+}
+push $GitUrl1
+push $GitUrl2
+
 echo "Exec finish, Ctrl+C to exit!"
 sleep 1000
