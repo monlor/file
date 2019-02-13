aac61bf0fb3e6c6b8bd84dcf5848cc02ea97af62
monlor <monlor@qq.com>
2019-02-13 11:43:45 +0800

    2019-02-13

--- push.sh
+++ push.sh
@@ -8,14 +8,14 @@ ProjectName="$(echo $ProjectPath | sed -e "s|/.*/||")"
 echo "Add files to html list..."
 ./packhtml.sh
 
+find . -name '.DS_Store' | xargs rm -rf
+git add .
+git commit -m "`date +%Y-%m-%d`"
+
 function push() {
 	GitUrl="$1"
 	[ -z "$ProjectName" ] && echo "Null Project Name!" && exit
 	echo "Push [$ProjectName] To [$GitUrl/$ProjectName.git]."
-	cd "$ProjectPath"
-	find . -name '.DS_Store' | xargs rm -rf
-	git add .
-	git commit -m "`date +%Y-%m-%d`"
 	git remote rm origin
 	git remote add origin "$GitUrl"/"$ProjectName".git
 	git push -u origin master -f
